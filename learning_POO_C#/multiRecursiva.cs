bc7976337a177f777461a019c72413ba295a774e
davilucas156 <142557764+davilucas156@users.noreply.github.com>
2026-02-27 20:38:13 -0300

    recursao de multiplos de N

--- learning_POO_C#/multiRecursiva.cs
+++ learning_POO_C#/multiRecursiva.cs
@@ -1,28 +1,28 @@
-using System;
+// using System;
 
-class Program
-{
-    public static int SomaRecursiva(int Num, int Multiplicador)
-    {
-        if (Multiplicador == 0)
-            return 0;
+// class Program
+// {
+//     public static int SomaRecursiva(int Num, int Multiplicador)
+//     {
+//         if (Multiplicador == 0)
+//             return 0;
       
-        return Num + SomaRecursiva(Num, Multiplicador - 1) ;
-    }
+//         return Num + SomaRecursiva(Num, Multiplicador - 1) ;
+//     }
 
-    public static void Main(string[] args)
-    {
-        int Num = 0;
-        int Multiplicador = 0;
+//     public static void Main(string[] args)
+//     {
+//         int Num = 0;
+//         int Multiplicador = 0;
 
-        Console.WriteLine("escreva um numeros a multiplicar ");
-        Num = int.Parse(Console.ReadLine());
+//         Console.WriteLine("escreva um numeros a multiplicar ");
+//         Num = int.Parse(Console.ReadLine());
 
-        Console.WriteLine("escreva um numeros a multiplicar ");
-        Multiplicador = int.Parse(Console.ReadLine());
+//         Console.WriteLine("escreva um numeros a multiplicar ");
+//         Multiplicador = int.Parse(Console.ReadLine());
 
-        Console.WriteLine ("o resultado é : " + SomaRecursiva(Num, Multiplicador));
+//         Console.WriteLine ("o resultado é : " + SomaRecursiva(Num, Multiplicador));
         
     
-    }
-}
+//     }
+// }
--- learning_POO_C#/multiplosN.cs
+++ learning_POO_C#/multiplosN.cs
@@ -1,16 +1,33 @@
-using System; 
+using System;
 
-class Program{
-    static int multiplosDeN(int n, int q)
+
+class Program2
+{
+    static void MultiplosDeN(int n, int q)
     {
-        return 
+
+        if (q == 0)
+            return;
+
+        MultiplosDeN(n, q - 1);
+        Console.WriteLine($"{n} x {q} = {n*q} ");
+      
+
     }
     static void Main(string[] args)
     {
         int n = 0;
         int q = 0;
 
- Console.WriteLine("escreva umudanca");
+        Console.WriteLine("escreva o valor para : n");
+        n = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("escreva o valor para : q");
+        q = int.Parse(Console.ReadLine());
+
+        Console.WriteLine($"os numeros multiplos de {n}");
+        MultiplosDeN(n, q);
+
     }
 
 }
